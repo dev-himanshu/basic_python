2a8b294914ab33205419292f2a5c63070bb123c8
Himanshu Mishra <dev.himanshumishra@gmail.com>
2021-02-05 15:19:38 +0530

    venv

--- BasicPythonConcept/venv/Scripts/activate.bat
+++ BasicPythonConcept/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\himanshu\Documents\GitHub\basic_python\BasicPythonConcept\venv
+set VIRTUAL_ENV=G:\BASIC_FUNDAMENTALS_OF_PYTHON\basic_python\BasicPythonConcept\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
